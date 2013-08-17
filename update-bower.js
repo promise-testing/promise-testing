90deaa72ddaa1d8d565e5d8d4b5f06bf062bdfac
James Talmage <james.talmage@jrtechnical.com>
2013-08-17 02:28:36 -0400

    Updating build script

--- update-bower.js
+++ update-bower.js
@@ -1,7 +1,13 @@
 var bower = require('./bower.json');
 var pkg = require('./package.json');
+var component = require('./component.json');
 if(bower.version != pkg.version){
     var fs = require('fs');
     bower.version = pkg.version;
     fs.writeFileSync('./bower.json',JSON.stringify(bower,2,2));
+}
+if(component.version != pkg.version){
+    var fs = require('fs');
+    component.version = pkg.version;
+    fs.writeFileSync('./component.json',JSON.stringify(component,2,2));
 }
